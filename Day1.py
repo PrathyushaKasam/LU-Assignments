3991b131b8234120fa1ae0a97dac9266c032155e
Prathyusha Kasam <51985972+PrathyushaKasam@users.noreply.github.com>
2021-06-30 22:10:31 +0530

    Day 1 Assignment submitted

---
+++ Day1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number "))
+b = int(input("Enter the second number "))
+sum = a + b
+print("The sum is ", sum)
+sub = a - b
+print("The difference is ", sub)
+mul = a * b
+print("The product is ", mul)
+div = a / b
+print("The quotient is ", div)
